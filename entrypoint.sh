02766cd3d92ff8527583140ea9cf3f2605c6623c
Spencer Young <spencer.young@spyoung.com>
2019-05-10 20:46:21 -0700

    add credential helper

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 branch=$(git symbolic-ref --short HEAD)
 sh -c "git config --global user.name $GIT_USERNAME"
-sh -c "git config --global user.password $GIT_PASSWORD"
+sh -c "git config --global core.askPass /cred-helper.sh"
+sh -c "git config --global credential.helper cache"
+sh -c "git config credential"
 sh -c "git remote add mirror $*"
 sh -c "git remote set-url $*"
 sh -c "echo pushing to $(git remote get-url --push mirror)"
