e2090ac832b2a1e41af75bdcb59e5c1be723dd29
Spencer Young <spencer.young@spyoung.com>
2019-05-10 21:18:19 -0700

    cleanup entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+set -eu
+
 branch=$(git symbolic-ref --short HEAD)
 sh -c "git config --global credential.username $GIT_USERNAME"
 sh -c "git config --global core.askPass /cred-helper.sh"
 sh -c "git config --global credential.helper cache"
-sh -c "git config credential"
 sh -c "git remote add mirror $*"
 sh -c "git remote set-url $*"
 sh -c "echo pushing to $(git remote get-url --push mirror)"
