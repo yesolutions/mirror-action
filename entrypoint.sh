34b234f12241951e792723434861eb8b7c94d51c
perkoren <rwojcicka@gmail.com>
2020-03-31 01:09:45 +0200

    fixing 'fatal: ref HEAD is not a symbolic ref' error

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ branch=$(git symbolic-ref --short HEAD)
 sh -c "git config --global credential.username $GIT_USERNAME"
 sh -c "git config --global core.askPass /cred-helper.sh"
 sh -c "git config --global credential.helper cache"
-sh -c "git remote add mirror $*"
 sh -c "git checkout $GITHUB_REF"
+sh -c "git remote add mirror $*"
 sh -c "echo pushing to $branch branch at $(git remote get-url --push mirror)"
 sh -c "git push mirror $branch"
