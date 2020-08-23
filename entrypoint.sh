f6ed410eb078ac550c1d19b94766c7d0bc1d1acd
Spencer Phillip Young <spencer.young@spyoung.com>
2020-08-23 01:06:00 -0700

    use echo -e

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,8 +35,8 @@ git config --global credential.username "${GIT_USERNAME}"
 
 if [[ "${GIT_SSH_PRIVATE_KEY}" -ne "" ]]; then
     mkdir ~/.ssh
-    echo "${INPUT_SSH_PRIVATE_KEY}" > ~/.ssh/id_rsa
-    echo "${INPUT_SSH_PUBLIC_KEY}" > ~/.ssh/id_rsa.pub
+    echo -e "${INPUT_SSH_PRIVATE_KEY}" > ~/.ssh/id_rsa
+    echo -e "${INPUT_SSH_PUBLIC_KEY}" > ~/.ssh/id_rsa.pub
     chmod 600 ~/.ssh/id_rsa
     chmod 600 ~/.ssh/id_rsa.pub
 else
