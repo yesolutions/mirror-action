5b2e1b36cace472cdca9945c48739936bed9830a
Spencer Young <spencer.young@spyoung.com>
2020-08-23 02:07:17 -0700

    host verification and identity fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,8 @@ REMOTE=${INPUT_REMOTE:-"$*"}
 GIT_SSH_PRIVATE_KEY=${INPUT_GIT_SSH_PRIVATE_KEY}
 GIT_SSH_PUBLIC_KEY=${INPUT_GIT_SSH_PUBLIC_KEY}
 GIT_PUSH_ARGS=${INPUT_GIT_PUSH_ARGS:-"--tags --force --prune"}
-
+GIT_SSH_NO_VERIFY_HOST=${INPUT_GIT_SSH_NO_VERIFY_HOST}
+GIT_SSH_KNOWN_HOSTS=${INPUT_GIT_SSH_KNOWN_HOSTS}
 HAS_CHECKED_OUT="$(git rev-parse --is-inside-work-tree 2>/dev/null || /bin/true)"
 
 
@@ -40,11 +41,18 @@ if [[ "${GIT_SSH_PRIVATE_KEY}" != "" ]]; then
     echo "${GIT_SSH_PUBLIC_KEY}" > ~/.ssh/id_rsa.pub
     chmod 600 ~/.ssh/id_rsa
     chmod 600 ~/.ssh/id_rsa.pub
-    if [[ "${DEBUG}" -eq "true" ]]; then
-        stat ~/.ssh/id_rsa
-        stat ~/.ssh/id_rsa.pub
-        wc ~/.ssh/id_rsa
-        wc ~/.ssh/id_rsa.pub
+    if [[ "${GIT_SSH_KNOWN_HOSTS}" != "" ]]; then
+      echo "${GIT_SSH_KNOWN_HOSTS}" > ~/.ssh/known_hosts
+      git config --global core.sshCommand "ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=~/.ssh/known_hosts"
+    else
+      if [[ "${GIT_SSH_NO_VERIFY_HOST}" != "true" ]]; then
+        echo "WARNING: no known_hosts set and host verification is enabled (the default)"
+        echo "WARNING: this job will fail due to host verification issues"
+        echo "Please either provide the GIT_SSH_KNOWN_HOSTS or GIT_SSH_NO_VERIFY_HOST inputs"
+        exit 1
+      else
+        git config --global core.sshCommand "ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+      fi
     fi
 else
     git config --global core.askPass /cred-helper.sh
