c427403fc79b38367cae100dbc0ea15c827e666e
Spencer Phillip Young <spencer.young@spyoung.com>
2020-08-23 00:48:39 -0700

    permissions for public key

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,6 +38,7 @@ if [[ "${GIT_SSH_PRIVATE_KEY}" -ne "" ]]; then
     echo "${INPUT_SSH_PRIVATE_KEY}" > ~/.ssh/id_rsa
     echo "${INPUT_SSH_PUBLIC_KEY}" > ~/.ssh/id_rsa.pub
     chmod 600 ~/.ssh/id_rsa
+    chmod 600 ~/.ssh/id_rsa.pub
 else
     git config --global core.askPass /cred-helper.sh
     git config --global credential.helper cache
