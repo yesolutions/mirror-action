10b81357f9c92f09c2536b61b78d0f3ed5da18ff
Spencer Phillip Young <spencer.young@spyoung.com>
2020-08-23 02:26:11 -0700

    Ssh (#9)
    * host verification and identity fixes
    * adds support for KNOWN_HOSTS
    * adds support to skip host verification

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,10 @@ fi
 GIT_USERNAME=${INPUT_GIT_USERNAME:-${GIT_USERNAME:-"git"}}
 REMOTE=${INPUT_REMOTE:-"$*"}
 GIT_SSH_PRIVATE_KEY=${INPUT_GIT_SSH_PRIVATE_KEY}
+GIT_SSH_PUBLIC_KEY=${INPUT_GIT_SSH_PUBLIC_KEY}
 GIT_PUSH_ARGS=${INPUT_GIT_PUSH_ARGS:-"--tags --force --prune"}
-
+GIT_SSH_NO_VERIFY_HOST=${INPUT_GIT_SSH_NO_VERIFY_HOST}
+GIT_SSH_KNOWN_HOSTS=${INPUT_GIT_SSH_KNOWN_HOSTS}
 HAS_CHECKED_OUT="$(git rev-parse --is-inside-work-tree 2>/dev/null || /bin/true)"
 
 
@@ -32,10 +34,26 @@ fi
 git config --global credential.username "${GIT_USERNAME}"
 
 
-if [[ "${GIT_SSH_PRIVATE_KEY}" -ne "" ]]; then
+if [[ "${GIT_SSH_PRIVATE_KEY}" != "" ]]; then
     mkdir ~/.ssh
-    echo "${INPUT_SSH_PRIVATE_KEY}" > ~/.ssh/id_rsa
+    chmod 700 ~/.ssh
+    echo "${GIT_SSH_PRIVATE_KEY}" > ~/.ssh/id_rsa
+    echo "${GIT_SSH_PUBLIC_KEY}" > ~/.ssh/id_rsa.pub
     chmod 600 ~/.ssh/id_rsa
+    chmod 600 ~/.ssh/id_rsa.pub
+    if [[ "${GIT_SSH_KNOWN_HOSTS}" != "" ]]; then
+      echo "${GIT_SSH_KNOWN_HOSTS}" > ~/.ssh/known_hosts
+      git config --global core.sshCommand "ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=~/.ssh/known_hosts"
+    else
+      if [[ "${GIT_SSH_NO_VERIFY_HOST}" != "true" ]]; then
+        echo "WARNING: no known_hosts set and host verification is enabled (the default)"
+        echo "WARNING: this job will fail due to host verification issues"
+        echo "Please either provide the GIT_SSH_KNOWN_HOSTS or GIT_SSH_NO_VERIFY_HOST inputs"
+        exit 1
+      else
+        git config --global core.sshCommand "ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+      fi
+    fi
 else
     git config --global core.askPass /cred-helper.sh
     git config --global credential.helper cache
