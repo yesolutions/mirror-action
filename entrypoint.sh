4d62d459d6bc3c1d7a554009442987c329a40b54
Spencer Phillip Young <spencer.young@spyoung.com>
2020-08-23 01:41:22 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,8 +36,8 @@ git config --global credential.username "${GIT_USERNAME}"
 if [[ "${GIT_SSH_PRIVATE_KEY}" != "" ]]; then
     mkdir ~/.ssh
     chmod 700 ~/.ssh
-    printf "%s" "${GIT_SSH_PRIVATE_KEY}" > ~/.ssh/id_rsa
-    printf "%s" "${GIT_SSH_PUBLIC_KEY}" > ~/.ssh/id_rsa.pub
+    echo "${GIT_SSH_PRIVATE_KEY}" > ~/.ssh/id_rsa
+    echo "${GIT_SSH_PUBLIC_KEY}" > ~/.ssh/id_rsa.pub
     chmod 600 ~/.ssh/id_rsa
     chmod 600 ~/.ssh/id_rsa.pub
     if [[ "${DEBUG}" -eq "true" ]]; then
